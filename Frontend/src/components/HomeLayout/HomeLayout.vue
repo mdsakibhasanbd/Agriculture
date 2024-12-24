86c2a85fb4510ad6cb89eefd5cbaca9ed7b22a3d
mdsakibhasanbd <mdsakibhasanbd948@gmail.com>
2024-12-25 01:21:41 +0600

    Nav and Banner add

---
+++ Frontend/src/components/HomeLayout/HomeLayout.vue
@@ -0,0 +1,6 @@
+<template>
+    <!-- <div class=" container   mx-auto max-w-screen-xl text-gray-700 overflow-hidden"> -->
+    <div class="container px-4 md:px-12 lg:px-8 mx-auto max-w-screen-xl text-gray-700 overflow-hidden">
+      <slot> </slot>
+    </div>
+  </template>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <Nav />
+        <HomePage />
+        <HomeLayout>
+        </HomeLayout>
+    </main>
+</template>
+
+<script>
+import HomeLayout from '@/components/HomeLayout/HomeLayout.vue';
+import Nav from '@/components/Nav.vue';
+import HomePage from '@/components/Home/HomePage.vue';
+    export default {
+        components: {
+            HomeLayout,
+            Nav,
+            HomePage
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
